5b10ab8adcdbfc2d2d9c089d82c5908139239107
Aadithya U <aadi1194@gmail.com>
2014-04-17 20:12:33 +0530

    Update maxwoods.cpp

--- maxwoods.cpp
+++ maxwoods.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #define sd(x) scanf("%d",&x) 
 #define RIGHT 1
-#define LEFT 2
+#define LEFT 0
 using namespace std;
 
 int m,n;
@@ -12,26 +12,14 @@ char trees[1000][1000];
 int dp[1000][1000][3];
 
 int findmax(int i , int j , int direction) {
-
 	//base condition.. it cannot move further when the following conditions are met
 	if(j < 0 || j >= n || i >= m || i < 0 || trees[i][j] == '#' )
 		return 0;
-		
 	//if not empty.. return 
 	if(dp[i][j][direction] != -1) return dp[i][j][direction];
-	
-	if(trees[i][j] == '0') {
-		if(direction == LEFT) 
-			return dp[i][j][LEFT] = max(findmax(i+1 ,j , RIGHT),findmax(i , j-1 , LEFT));
-		else 
-			return dp[i][j][LEFT] = max(findmax(i+1 , j, LEFT),findmax(i , j+1 , RIGHT));
-	}
-	if(trees[i][j] == 'T') {
-		if(direction == LEFT) 
-			return dp[i][j][RIGHT] = max(findmax(i+1 ,j , RIGHT),findmax(i , j-1 , LEFT)) + 1;
-		else 
-			return dp[i][j][RIGHT] = max(findmax(i+1 , j, LEFT),findmax(i , j+1 , RIGHT)) + 1;
-	}
+	int c = 0; 
+	if(trees[i][j] == 'T') c++;
+	return dp[i][j][LEFT] = max(findmax(i+1 ,j , 1 - direction),findmax(i , j-1 , direction)) + c;
 }
 
 int main() {
